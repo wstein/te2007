6b1a895b647620e7a67b0313a7af5d68e89ad397
Werner Stein <wstein@avaya.com>
2020-07-22 10:46:36 +0200

    Aufgabe 1

---
+++ # Template engine course 2020-07-22.md
@@ -0,0 +1,10 @@
+# Template engine course 2020-07-22
+
+## getting started
+
+```ps1
+cd C:\xampp\htdocs
+git clone https://github.com/wstein/te2007.git
+```
+
+open 
---
+++ Aufgabe 1/kontaktformular.php
@@ -0,0 +1,61 @@
+<html>
+   <body>
+      <?php
+      $vorname= $_POST["T_KndVorname"];
+      $name= $_POST["T_KndName"];
+      $plz= $_POST["T_KndPLZ"];
+      $ort= $_POST["T_KndOrt"];
+      $strasse= $_POST["T_KndStrasse"];
+      $hausnr= $_POST["T_KndHausNr"];
+      $email= $_POST["T_Email"];
+      $kommentar= $_POST["TA_Nachricht"];
+      $produkte= $_POST["produkte"];
+      $anrede= $_POST["PD_Anrede"];
+       echo "<h2>Sie haben die folgenden Daten &uuml;bermittelt:</h2>
+             <table border=\"1\">
+			     <tr>
+                <td>Produkt</td>
+                <td> $produkte</td>
+             </tr>
+			    <tr>
+                <td>Anrede</td>
+                <td> $anrede </td>
+             </tr>
+             
+             <tr>
+                <td>Vorname</td>
+                <td> $vorname </td>
+             </tr>
+             <tr>
+                <td>Nachname</td>
+                <td> $name</td>
+             </tr>
+             <tr>
+                <td>PLZ</td>
+                <td> $plz </td>
+             </tr>
+             <tr>
+                <td>Ort</td>
+                <td> $ort </td>
+             </tr>
+             <tr>
+                <td>Strasse</td>
+                <td> $strasse </td>
+             </tr>
+             <tr>
+                <td>Hausnummer</td>
+                <td> $hausnr</td>
+             </tr>
+             <tr>
+                <td>Email</td>
+                <td> $email</td>
+             </tr>
+             <tr>
+                <td>Bemerkung</td>
+                <td> $kommentar</td>
+             </tr>
+            
+			 </table>";
+      ?>
+   </body>
+</html>
